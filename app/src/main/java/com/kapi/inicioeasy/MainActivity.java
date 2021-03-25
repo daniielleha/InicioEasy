673248554f5d09a41c44d736d5c03ddb365cb064
daniielleha <juan_ha@tesch.edu.mx>
2021-03-25 11:52:12 -0600

    archivo cambiado 3

--- app/src/main/java/com/kapi/inicioeasy/MainActivity.java
+++ app/src/main/java/com/kapi/inicioeasy/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
@@ -18,12 +19,15 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import es.dmoral.toasty.Toasty;
+
 public class MainActivity extends AppCompatActivity {
 
     ParseUser parseUser =ParseUser.getCurrentUser();
     private static final String TAG = MainActivity.class.getSimpleName();
     private MaterialButton tst;
     private EditText correo;
+    private TextView welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,40 +36,62 @@ public class MainActivity extends AppCompatActivity {
 
         correo = (EditText) findViewById(R.id.etEmail);
         tst = (MaterialButton) findViewById(R.id.btnIniciar);
+        welcome = findViewById(R.id.tvWelcome);
         tst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                 if (correo != null) {/*(currentUser != null){
+                /*ParseUser currentUser = ParseUser.getCurrentUser();
+                if (currentUser != null){
                     String userId = currentUser.getObjectId();
                     String username = currentUser.getUsername();
                     String objectID = currentUser.getObjectId();
                 } else {
                     Toast.makeText(getApplicationContext(), "the user does not exist!",Toast.LENGTH_SHORT).show();
                 }*/
-                HashMap<String, Object> params = new HashMap<String, Object>();
-                /*try {
+                if (correo != null) {
+
+
+                    HashMap<String, Object> params = new HashMap<String, Object>();
+                    /*try {*/
                     params.put("correo", correo.getText().toString());
-                    //Log.d(TAG, "onClick: "+ String.valueOf(parseUser));
+                /*    //Log.d(TAG, "onClick: "+ String.valueOf(parseUser));
                 } catch (Exception e){
                     e.printStackTrace();
                 }*/
-                     params.put("email", correo);
-                     ParseCloud.callFunctionInBackground("getUserNameByEmail", params, new FunctionCallback() {
+                    ParseCloud.callFunctionInBackground("getUserNameByEmail", params, new FunctionCallback() {
 
-                    @Override
-                    public void done(Object object, ParseException e) {
-                        Log.d(TAG, String.valueOf(object));
-                    }
+                        @Override
+                        public void done(Object object, ParseException e) {
+                            Log.d(TAG, String.valueOf(object));
+                        }
 
-                    @Override
-                    public void done(Object o, Throwable throwable) {
-                        Log.d("---->testKapital+", String.valueOf(o));
-                        HashMap result = (HashMap) o;
-                        int code = Integer.parseInt(String.valueOf(result.get("code")));
-                        if (code == 200) {
-                            HashMap response = (HashMap) result.get("response");
-                            Log.d(TAG, "done: " + response.toString());
+                        @Override
+                        public void done(Object o, Throwable throwable) {
+                            Log.d(TAG, String.valueOf(o));
+                            if (o != null) {
+                                HashMap result = (HashMap) o;
+                                int code = Integer.parseInt(String.valueOf(result.get("code")));
+                                if (code == 200 && result.get("response") instanceof ArrayList) {
+
+                                    ArrayList response = (ArrayList) result.get("response");
+                                    if (response.size() > 0) {
+                                        HashMap name = (HashMap) response.get(0);
+                                        welcome.setText("Hola, " + (String) name.get("nombre"));
+
+                                    } else {
+                                        Toasty.error(getApplicationContext(), "No se pudo obtener la información, intenta nuevamente", Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    welcome.setText("Hola");
+                                }
+
+                            }
+                        }
+                    });
+                }
+
+                            //HashMap response = (HashMap) result.get("response");
+                            //Log.d(TAG, "done: " + response.toString());
                             /*for (int i = 0; i < response.size(); i++) {
                                 HashMap responsetmp = (HashMap) response.get(i);
                                 Log.d(TAG, "done: " + response.get(i));
@@ -73,12 +99,11 @@ public class MainActivity extends AppCompatActivity {
                                     planes = (ArrayList) responsetmp.get("plans");
                                 }
                             }*/
-                        } else if (code == 400) {
+                      /*  } *//*else if (code == 400) {
                             Toast.makeText(getApplicationContext(), (CharSequence) result.get("msg"), Toast.LENGTH_LONG).show();
-                        }
+                        }*//*
                     }
-                });
+                */}});
+
             }
-        });
-    }
-}
+        }
